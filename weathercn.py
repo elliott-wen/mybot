ef6e42878c275b11702b835ec211e6879ceac9d9
elliottwen <jq.elliott.wen@gmail.com>
2016-09-02 00:21:30 +1200

    new commit

--- weathercn.py
+++ weathercn.py
@@ -5,6 +5,7 @@
 from selenium import webdriver
 from plugin import *
 import traceback
+from PIL import Image
 class WeatherCN(Plugin):
 
     def gen_message(self, caller_id = None, message = None):
@@ -14,11 +15,19 @@ class WeatherCN(Plugin):
             driver.get('http://m.weathercn.com/index.do?id=101010100&partner=m')
             url = self.filename_generator("jpg")
             driver.save_screenshot(url) # save a screenshot to disk
+            section = driver.find_element_by_css_selector("div.header-info")
+            l =  section.location
+            r = [l['x'], l['y'], 1000, 1300]
+            im = Image.open(url)
+            region = im.crop(r)
+            non_transparent=Image.new('RGBA',region.size,(255,255,255,255))
+            non_transparent.paste(region,(0,0),region)
+            non_transparent.save(url)
             return {"msg":u"小琪子天气预报", "pic":url}
         except:
             traceback.print_exc()
             return None
 
 
-
+WeatherCN().gen_message()
 
--- weathernz.py
+++ weathernz.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from plugin import *
 import traceback
-
+from PIL import Image
 
 class WeatherNZ(Plugin):
     def gen_message(self, caller_id = None, message = None):
@@ -9,10 +9,18 @@ class WeatherNZ(Plugin):
             driver = webdriver.PhantomJS() # or add to your PATH
             driver.set_window_size(1024, 768) # optional
             driver.get('http://www.metservice.com/towns-cities/wellington/wellington-city')
+            section = driver.find_element_by_css_selector("#day6to10-disclaimer")
+            l =  section.location
+
+            r = [l['x'], l['y'], 650, 1250]
             url = self.filename_generator("jpg")
             driver.save_screenshot(url) # save a screenshot to disk
+            im = Image.open(url)
+            region = im.crop(r)
+            region.save(url)
             return {"msg":"Weather Forecast", "pic":url}
         except:
             traceback.print_exc()
             return None
 
+# WeatherNZ().gen_message()
