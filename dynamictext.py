cfacbd9a8da62e7cfd1f10d2b1918defe034f2a7
elliottwen <jq.elliott.wen@gmail.com>
2016-09-22 20:57:05 +0800

    msg

--- dynamictext.py
+++ dynamictext.py
@@ -21,7 +21,7 @@ class DynamicText(Plugin):
             canvas_height = font_height*2
             img = Image.new("RGB", (canvas_width, canvas_height), (255, 255, 255)) # 创建图形
             draw = ImageDraw.Draw(img) # 创建画笔
-            draw.text((0, canvas_height/4), canvas_width/3, font=font, fill=(random.randint(0,30), random.randint(0,30), random.randint(0,30)))
+            draw.text((canvas_height/3, canvas_height/4), strs, font=font, fill=(random.randint(0,30), random.randint(0,30), random.randint(0,30)))
             current_pos = 0
             imgs = []
             if windows_size > canvas_width:
