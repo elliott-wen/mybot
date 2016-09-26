a5c8e731d7bb4fab74f7bc49d6903befe10081bf
elliottwen <jq.elliott.wen@gmail.com>
2016-09-26 16:58:05 +1300

    newcommit

--- database.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'elliott'
-
-import sqlite3
-conn = sqlite3.connect("record.db")
-c = conn.cursor()
-c.execute('''insert into records (username, record_text, record_date) values('test1', 'test2', 'test' )''')
-conn.commit()
-c.execute('''select * from records''')
-print c.fetchall()
